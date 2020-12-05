8dccceb4af8c6fa0b3ecb409b4343ad250b16d85
Clement IACOBUCCI <clement.iacobucci@gmail.com>
2020-12-05 14:04:04 +0100

    feat(bootstap.php): ajout des infos de la bdd

--- Back/bootstrap.php
+++ Back/bootstrap.php
@@ -6,11 +6,11 @@ require_once "vendor/autoload.php";
 $isDevMode = true;
 $config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
 $conn = array(
-'host' => 'ec2-3-248-4-172.eu-west-1.compute.amazonaws.com',
+'host' => 'ec2-54-75-248-49.eu-west-1.compute.amazonaws.com',
 'driver' => 'pdo_pgsql',
-'user' => 'xqcmspzrocvvtt',
-'password' => 'a6479131a452d0da96fe9ae196d0993d3e9ac92e79d663702f46271d5ebff331',
-'dbname' => 'd8jrr50unmfhsl',
+'user' => 'jqpmazbnthdlos',
+'password' => '289aad4eb636a944c36842d9a823f9185e72ef9dc8bd1c45a5ae2c87d86c50cb',
+'dbname' => 'd5kptskho1u01',
 'port' => '5432'
 );
 $entityManager = EntityManager::create($conn, $config);
